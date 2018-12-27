cb887f265d1f390bbce82876df7abbc695f3ff40
chagoy <angelochagoy@gmail.com>
2018-12-27 14:26:51 -0800

    switching ports

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/baseball';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/jwt-auth-demo';
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 5000;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'the_secret_key';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '15m';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:8080';
