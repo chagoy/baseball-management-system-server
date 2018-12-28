fe9b5fbac3706e560feef3e422e23156eb4e0a19
chagoy <angelochagoy@gmail.com>
2018-12-27 16:30:26 -0800

    changing base url again

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://mpkbaseball.com/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://mpkbaseball.com';
 export const STRIPE_KEY = 'pk_live_xGFyFfdus6XWUYDKDzY0HizQ';
