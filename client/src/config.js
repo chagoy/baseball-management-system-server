598f12fd2a8142f6c4430326f8acd29c449d7f3c
chagoy <angelochagoy@gmail.com>
2019-02-26 20:53:36 -0800

    another build

--- client/src/config.js
+++ client/src/config.js
@@ -1,9 +1,9 @@
 // live
-// export const API_BASE_URL = 'https://mpkbaseball.com';
-// export const REACT_APP_API_BASE_URL = 'https://mpkbaseball.com';
-// export const STRIPE_KEY = 'pk_live_xGFyFfdus6XWUYDKDzY0HizQ';
+export const API_BASE_URL = 'https://mpkbaseball.com';
+export const REACT_APP_API_BASE_URL = 'https://mpkbaseball.com';
+export const STRIPE_KEY = 'pk_live_xGFyFfdus6XWUYDKDzY0HizQ';
 
 // dev
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
-export const STRIPE_KEY = 'pk_test_ri1za32qj1cz2Li5eTdq0UwT';
-export const REACT_APP_SERVER_URL = 'http://localhost:5000';
+// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+// export const STRIPE_KEY = 'pk_test_ri1za32qj1cz2Li5eTdq0UwT';
+// export const REACT_APP_SERVER_URL = 'http://localhost:5000';
