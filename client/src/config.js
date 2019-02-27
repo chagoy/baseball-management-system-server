624df7521c971c69379b51f2949161b8d4072fdc
chagoy <angelochagoy@gmail.com>
2019-02-26 18:38:13 -0800

    prod push

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 // live
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://mpkbaseball.com';
+export const API_BASE_URL = 'https://mpkbaseball.com';
 export const STRIPE_KEY = 'pk_live_xGFyFfdus6XWUYDKDzY0HizQ';
 
 // dev
